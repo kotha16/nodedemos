fbd652f2b65a45540fb11691198085f116912a6d
kotha16@outlook.com <kotha16@outlook.com>
2015-09-10 22:58:34 -0400

    vash

--- NodejsWebApp1/NodejsWebApp1/server.js
+++ NodejsWebApp1/NodejsWebApp1/server.js
@@ -1,8 +1,20 @@
-﻿var http = require('http');
-var _ = require('underscore');
+﻿
+var http = require('http');
+//var _ = require('underscore');
 
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
+
+var express = require("express");
+var app = express();
+app.set("view engine", "vash");
+app.get("/", function (req, res) {
+    //res.send("<html><head></head><body><h1>Express</h1></body></html>");
+    res.render("index", { title: 'express' });
+});
+
+var server = http.createServer(app);
+server.listen(port);
